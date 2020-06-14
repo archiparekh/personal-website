7a9da2a02e0d1177d01a499fce7c64c7a1e1711d
Archi Parekh <archiparekh23@gmail.com>
2020-06-14 11:28:25 -0400

    Updates

---
+++ precache-manifest.afb1b8f1d784a0bd358db81d584f09df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898e0a1440e3e03ec1753beb2dae6a70",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "c38d514ab056d1871813",
+    "url": "/personal-website/static/css/2.899cae31.chunk.css"
+  },
+  {
+    "revision": "fc314710b830d8bd4111",
+    "url": "/personal-website/static/css/main.15e6ee29.chunk.css"
+  },
+  {
+    "revision": "c38d514ab056d1871813",
+    "url": "/personal-website/static/js/2.c90804de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/personal-website/static/js/2.c90804de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc314710b830d8bd4111",
+    "url": "/personal-website/static/js/main.d3d92afb.chunk.js"
+  },
+  {
+    "revision": "f50455ddb69baf523794",
+    "url": "/personal-website/static/js/runtime-main.3a9d5e67.js"
+  },
+  {
+    "revision": "69b2310569ee41a89f42056b9fb35018",
+    "url": "/personal-website/static/media/Archi-Parekh-Resume.69b23105.pdf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a7756ffb88a781cdfaacd669c17c5eb7",
+    "url": "/personal-website/static/media/me2.a7756ffb.jpg"
+  }
+]);
